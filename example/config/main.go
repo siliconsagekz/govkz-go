0ce3f208d25781967f7799676f3ac27dec121bbb
bukhashov <bukhashov2301@mail.ru>
2023-10-10 15:30:14 +0600

    add dir services

--- example/config/main.go
+++ example/config/main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 
 	"github.com/siliconsagekz/govkz-go"
-	"github.com/siliconsagekz/govkz-go/internal/handler"
+	"github.com/siliconsagekz/govkz-go/services"
 )
 
 func main() {
 	govinit := govkzgo.Init()
 
-	go_h :=  handler.NewStat(govinit)
+	go_h :=  services.NewStat(govinit)
 
 	compone, err := go_h.GetInfoByIinOrBin("010123000150", "kz"); if err != nil {
 		fmt.Print(err)
--- services/stat.go
+++ services/stat.go
@@ -1,4 +1,4 @@
-package handler
+package services
 
 import (
 	"io"
